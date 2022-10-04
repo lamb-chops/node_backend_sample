49fc06a9eb8a207458450cbc5f32be034b6cce24
Joel Rivera <joel.rivera@endava.com>
2022-10-04 18:22:14 -0500

    feat: add a getBlog route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { getBlogs } = require('../controller/getBlogs')
 const { postBlog } = require('../controller/postBlog')
+const { getBlog } = require('../controller/getBlog')
 
 const router = express.Router()
 
-router.get('/blog/:id', getBlogs)
+router.get('/blog/:id', getBlog)
+router.get('/blogs', getBlogs)
 router.post('/blog/post', postBlog)
 
 module.exports = router
